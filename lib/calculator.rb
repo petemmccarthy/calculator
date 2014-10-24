0cd93f7375d0a22a650a281e4f9c745dcff8c6be
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2014-10-24 13:40:24 +0100

    subtraction added

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,4 +1,8 @@
 
-def adds_two_numbers(num_x, num_y)
+def add_two_numbers(num_x, num_y)
   num_x + num_y
+end
+
+def subtract_a_number_from_another(num_x, num_y)
+  num_x - num_y
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,14 +2,20 @@ require 'calculator'
 
 describe 'Calculator' do
 
-  it 'can add two numbers together' do
-    expect(adds_two_numbers(1,1)). to eq 2
+  it 'can add 1 and 1 together' do
+    expect(add_two_numbers(1,1)). to eq 2
   end
 
-  it 'can add another two numbers together' do
-    expect(adds_two_numbers(6,5)). to eq 11
+  it 'can add 6 and 5 together' do
+    expect(add_two_numbers(6,5)). to eq 11
   end  
 
+  it 'can subtract 1 from 3' do
+    expect(subtract_a_number_from_another(3,1)).to eq 2
+  end
 
+  it 'can subtract 5 from 9' do
+    expect(subtract_a_number_from_another(9,5)).to eq 4
+  end
 
 end
